5aa0703b92510556b5da5f3762f1ba068ac0fc1e
phpusr <phpusr@gmail.com>
2013-11-23 19:44:39 +0600

    move: Главную страницу в шаблон

--- js/myApp.js
+++ js/myApp.js
@@ -1,6 +1,15 @@
 'use strict';
 
-var app = angular.module('myApp', ['ngSanitize']);
+/* App Module */
+
+var app = angular.module('myApp', ['ngSanitize', 'ngRoute']);
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/index', {templateUrl: 'partials/index.html', controller: 'MyCtrl'})
+        .when('/test', {templateUrl: 'partials/test.html', controller: 'MyCtrl'})
+        .otherwise({redirectTo: '/index'});
+}]);
+
 app.controller('MyCtrl', function MyCtrl($scope) {
     $scope.title = 'test';
 });
---
+++ scripts/RunServer.bat
@@ -0,0 +1,2 @@
+cd ..
+node scripts/web-server.js
