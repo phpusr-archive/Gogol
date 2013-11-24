a69c66c16c69a7dce9e9bd1ea9f73d96628c759b
phpusr <phpusr@gmail.com>
2013-11-24 14:48:16 +0600

    add: Изменение стиля таба в зависимости от страницы

--- js/myApp.js
+++ js/myApp.js
@@ -5,17 +5,34 @@
 var app = angular.module('myApp', ['ngSanitize', 'ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
-        .when('/index', {templateUrl: 'partials/index.html', controller: 'MyCtrl'})
-        .when('/test', {templateUrl: 'partials/test.html', controller: 'MyCtrl'})
+        .when('/index', {templateUrl: 'partials/index.html', controller: 'TestCtrl'})
+        .when('/bio', {templateUrl: 'partials/test.html', controller: 'TestCtrl'})
+        .when('/photo', {templateUrl: 'partials/test.html', controller: 'TestCtrl'})
+        .when('/artworks', {templateUrl: 'partials/test.html', controller: 'TestCtrl'})
         .otherwise({redirectTo: '/index'});
 }]);
 
-app.controller('MyCtrl', function MyCtrl($scope, $location) {
-    console.log($location.$$path); //TODO узнавание ссылки
-    $scope.location = 'index';
+app.controller('MainCtrl', function MainCtrl($rootScope, $scope, $location) {
+
+    $scope.nav = [
+        {name: 'Главная', link: '#index', path: '/index'},
+        {name: 'Биография', link: '#bio', path: '/bio'},
+        {name: 'Фотогалерея', link: '#photo', path: '/photo'},
+        {name: 'Произведения', link: '#artworks', path: '/artworks'}
+    ];
+
+    //Смена страницы
+    $rootScope.changeActiveNav = function() {
+        var path = $location.$$path;
+
+        for(var i=0; i<$scope.nav.length; i++) {
+            var n = $scope.nav[i];
+            n.cl = n.path == path;
+        }
+    };
+
 });
 
-app.controller('TestCtrl', function MyCtrl($scope, $location) {
-    console.log($location.$$path); //TODO узнавание ссылки
-    $scope.location = 'test';
+app.controller('TestCtrl', function MyCtrl($rootScope, $scope, $location) {
+    $rootScope.changeActiveNav();
 });
