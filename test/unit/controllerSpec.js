e014288f1eba84949d32499127399e95d462ea54
phpusr <phpusr@gmail.com>
2013-12-13 17:44:04 +0600

    add: Два unit-теста для PhotoCtrl

--- test/unit/controllerSpec.js
+++ test/unit/controllerSpec.js
@@ -27,4 +27,26 @@ describe('App controllers', function() {
 
     });
 
+    describe('PhotoCtrl', function() {
+
+        var rootScope, scope, ctrl;
+
+        beforeEach(module('myApp'));
+
+        beforeEach(inject(function($controller) {
+            rootScope = {changeActiveNav: function(){}};
+            scope = {};
+            ctrl = $controller('PhotoCtrl', {$scope: scope, $rootScope: rootScope});
+        }));
+
+        it('should create "opt.nav" = thumbs', function() {
+            expect('thumbs').toBe(scope.opt.nav);
+        });
+
+        it('should create "opt.nav" = thumbs ', function() {
+            expect(400).toBe(scope.opt.height);
+        });
+
+    });
+
 });
