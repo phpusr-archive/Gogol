3abef0fa9994ce4826434ebecc684ecb3e9f0b51
phpusr <phpusr@gmail.com>
2013-12-13 19:28:01 +0600

    add: Тесты для биографии и фото

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -29,6 +29,32 @@ describe('Gogol App', function() {
         it('Должен найти 3 книги на главной странице', function() {
             expect(repeater('div.col-lg-4 h2').count()).toBe(3);
         });
+
+    });
+
+    describe('Bio view', function() {
+
+        beforeEach(function() {
+            browser().navigateTo('../../index.html#/bio');
+        });
+
+        it('Должен найти 5 пунктов биографии', function() {
+            expect(repeater('div.list-group a').count()).toBe(5);
+            expect(repeater('div h2').count()).toBe(5);
+        });
+
+    });
+
+    describe('Photo view', function() {
+
+        beforeEach(function() {
+            browser().navigateTo('../../index.html#/photo');
+        });
+
+        it('Должен найти 10 фотографий', function() {
+            expect(repeater('.fotorama img').count()).toBe(10);
+        });
+
     });
 
 });
